8277790d98f70c120496f62930a4106caf1a2fbd
Milancho Arsovski <milancho.arsovski@outlook.com>
2022-09-11 20:59:17 +0200

    sum_iznos_transakcii_prvo_tromesecje

--- bazi/Prakticen_Ispit_20220901.sql
+++ bazi/Prakticen_Ispit_20220901.sql
@@ -2,7 +2,8 @@ SELECT klient_id,
        banka_id,
        SUM(broj_smetki_sopstvenik) AS broj_smetki_sopstvenik,
        SUM(broj_smetki_ovlasteni_lica) AS broj_smetki_ovlasteni_lica,
-       SUM(broj_transakcii_prvo_tromesecje) AS broj_transakcii_prvo_tromesecje
+       SUM(broj_transakcii_prvo_tromesecje) AS broj_transakcii_prvo_tromesecje,
+       SUM(sum_iznos_transakcii_prvo_tromesecje) AS sum_iznos_transakcii_prvo_tromesecje
 FROM
 (
     -- broj smetki vo bankata za koi klientot e sopstvenik
@@ -19,7 +20,16 @@ FROM
             WHERE s1.klient_id = k.klient_id
                   AND s1.banka_id = b.banka_id
                   AND MONTH(t1.datum) IN ( 1, 2, 3 ) -- 1-vo tromesecje bilo koja godina
-        ) AS broj_transakcii_prvo_tromesecje
+        ) AS broj_transakcii_prvo_tromesecje,
+        (
+            SELECT SUM(t1.iznos)
+            FROM dbo.transakcii t1
+                JOIN dbo.smetki s1
+                    ON s1.smetka_br = t1.smetka_br
+            WHERE s1.klient_id = k.klient_id
+                  AND s1.banka_id = b.banka_id
+                  AND MONTH(t1.datum) IN ( 1, 2, 3 ) -- 1-vo tromesecje bilo koja godina
+        ) AS sum_iznos_transakcii_prvo_tromesecje
     FROM dbo.smetki s
         JOIN dbo.klienti k
             ON k.klient_id = s.klient_id
@@ -44,7 +54,18 @@ FROM
             WHERE ol1.klient_id = k.klient_id
                   AND s1.banka_id = b.banka_id
                   AND MONTH(t1.datum) IN ( 1, 2, 3 ) -- 1-vo tromesecje bilo koja godina
-        ) AS broj_transakcii_prvo_tromesecje
+        ) AS broj_transakcii_prvo_tromesecje,
+        (
+            SELECT SUM(t1.iznos)
+            FROM dbo.transakcii t1
+                JOIN dbo.ovlasteni_lica ol1
+                    ON ol1.smetka_br = t1.smetka_br
+                JOIN dbo.smetki s1
+                    ON s1.smetka_br = ol1.smetka_br
+            WHERE ol1.klient_id = k.klient_id
+                  AND s1.banka_id = b.banka_id
+                  AND MONTH(t1.datum) IN ( 1, 2, 3 ) -- 1-vo tromesecje bilo koja godina
+        ) AS sum_iznos_transakcii_prvo_tromesecje
     FROM dbo.ovlasteni_lica os
         JOIN dbo.smetki s
             ON s.smetka_br = os.smetka_br
